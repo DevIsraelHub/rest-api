7d8f1f4543f1e41c167caf28368bfee57d21285e
dev israel <devisrael01@gmail.com>
2024-04-15 22:47:33 +0100

    initial rest api commit

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { createUser, getUserByEmail } from "db/users";
+import { authentication, random } from "helpers";
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400)
+    }
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(400)
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      }
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
